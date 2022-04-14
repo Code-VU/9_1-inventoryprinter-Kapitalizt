8fdc976050d4c4f9456504665424b8f30addbf48
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-14 15:53:38 -0500

    1st

--- displayInventory.py
+++ displayInventory.py
@@ -2,5 +2,9 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
+    print("Inventory:")
+    for item in stuff:
+        print(stuff[item], item)
+    print(f"Total number of items: {sum(stuff.values())}")
 
-##displayInventory(stuff)
+displayInventory(stuff)
