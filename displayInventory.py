98f6d93dc6a2ae9be525d16f08ff0e9e7d7f4014
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-14 16:02:36 -0500

    4th

--- displayInventory.py
+++ displayInventory.py
@@ -1,6 +1,6 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
-def displayInventory(inventory):
+def displayInventory(stuff):
     # your code goes here
     print("Inventory:")
     for item in stuff:
